72e2e5969500fa9f9e1c11f893d69ac0d7a36466
ChumachenkoAngelika <chumachenko2005@icloud.com>
2022-10-24 22:07:58 +0300

    интенсив

---
+++ intensive/intensiv 22 oct.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+t = c - a
+p = d - b
+if -1 <= t <= 1 and -1 <= p <= 1:
+    print('YES')
+else:
+    print('NO')
--- intensive1/intensiv 25 0ct.py
+++
@@ -1,5 +0,0 @@
-x = int(input())
-if (x >= -30 and x <= -2) or ( x >= 7 and x<= 25):
-    print('YES')
-else:
-    print("NO")
